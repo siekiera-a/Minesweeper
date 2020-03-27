aee96b8f332b6becfe112a5b6c6394b44be93c99
siekiera-a <siekiera.a31@gmail.com>
2020-03-27 23:39:55 +0100

    Position class update

--- src/minesweeper/Position.java
+++ src/minesweeper/Position.java
@@ -1,8 +1,8 @@
 package minesweeper;
 
 public class Position {
-    final int x;
-    final int y;
+    public final int x;
+    public final int y;
 
     public Position(int x, int y) {
         this.x = x;
