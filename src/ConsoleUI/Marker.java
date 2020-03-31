0dce63912f924585735c0989efc42ad56585246f
siekiera-a <siekiera.a31@gmail.com>
2020-03-31 20:56:13 +0200

    Added class with field states markers

---
+++ src/ConsoleUI/Marker.java
@@ -0,0 +1,10 @@
+package ConsoleUI;
+
+public class Marker {
+
+    public final static char MINED = '*';
+    public final static char COVERED = '\u00B7'; // middle dot
+    public final static char DISCOVERED = ' ';
+    public final static char MARKED = 'X';
+
+}
